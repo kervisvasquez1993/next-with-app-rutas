2a11b178dedb9beebbbd75c237613240c043e129
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-03-10 17:48:30 -0400

    add style in project

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,18 +1,33 @@
+"use client";
+import { initialData } from "@/seed/seed";
 import notFound from "../not-found";
+import { ProductGrid, Title } from "@/components";
+import { useEffect, useState } from "react";
 
 interface Props {
-    params : {
-        id : string
-    }
+  params: {
+    id: string;
+  };
 }
-export default function({params } : Props) {
-    const {id} = params;
-    if(id == "kids"){
-        return  notFound();
-    }
-    return (
-        <div>
-            <h2>Category page {id} </h2>
-        </div>
-    )
+const products = initialData.products;
+export default function ({ params }: Props) {
+  const [category, setCategory] = useState([]);
+  const { id } = params;
+  if (id != "kid" && id != "men" && id != "women") {
+    return notFound();
+  }
+  useEffect(() => {
+    setCategory(products.filter((product) => product.gender == id));
+  }, [id]);
+  const label = {
+    men: "Para Hombres",
+    women: "Para Mujeres",
+    kid: "Para niños",
+  };
+  return (
+    <div>
+      <Title title={`Articulos para ${label[id]}`} />
+      <ProductGrid products={category} />
+    </div>
+  );
 }
--- src/components/ui/top-menu.tsx
+++ src/components/ui/top-menu.tsx
@@ -26,13 +26,13 @@ export const TopMenu = () => {
         </Link>
         <Link
           className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-          href={"/category/woman"}
+          href={"/category/women"}
         >
           Woman
         </Link>
         <Link
           className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-          href={"/category/kids"}
+          href={"/category/kid"}
         >
           Niños
         </Link>
